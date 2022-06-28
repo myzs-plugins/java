cc258bf79b53dc1de7239af71dd989e0330809a6
Kamontat Chantrachirathumrong <developer@kamontat.net>
2022-06-28 15:14:58 +0700

    chore(release): automatic commit process

--- app/java.sh
+++ app/java.sh
@@ -6,7 +6,7 @@ export _JAVA_HOME="/opt/homebrew/Cellar/openjdk/18.0.1.1/libexec/openjdk.jdk/Con
 export JAVA_BIN="/opt/homebrew/opt/openjdk/bin"
 
 if _myzs:internal:checker:folder-exist "$JAVA_BIN"; then
-  _myzs:internal:path-push "${JAVA_BIN}"
+  _myzs:internal:path-append "${JAVA_BIN}"
 fi
 
 if _myzs:internal:checker:folder-exist "$_JAVA_HOME"; then
